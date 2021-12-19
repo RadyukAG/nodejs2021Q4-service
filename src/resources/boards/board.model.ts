abf4065fc8b06971f59c01405212d529ecb969be
Alexey Radziuk <Alexey.Radziuk@coxautoinc.com>
2021-12-19 15:31:02 +0300

    refactor: add functions descriptions to boards functions

--- src/resources/boards/board.model.ts
+++ src/resources/boards/board.model.ts
@@ -4,6 +4,13 @@ import { IBoard, IDraftBoard } from './types';
 class Board {
     board: IBoard;
 
+/**
+ * Save a board in object. Also maps throw array of columns and assings id to each
+ * 
+ * @param param0.id - string id of a board
+ * @param param0.title - string title of a board
+ * @param param0.columns - columns, that belong to board (instances of IColumn)
+ */
     constructor({
         id = uuidv4(),
         title = 'Title',
@@ -19,6 +26,11 @@ class Board {
         }
     }
 
+/**
+ * Get a board object
+ * 
+ * @returns a board instance
+ */
     getBoard(): IBoard {
         return this.board;
     }
--- src/resources/boards/boards.service.ts
+++ src/resources/boards/boards.service.ts
@@ -2,22 +2,50 @@ import boardsRepo from './board.memory.repository';
 import Board from './board.model';
 import tasksRepo from '../tasks/task.memory.repository';
 import { IBoard, IDraftBoard } from './types';
-
-
-const addBoard = (board: IDraftBoard): IBoard | undefined | null => {
+/**
+ * Add board to repo
+ * 
+ * @param board - instance of IDraftBoard
+ * @returns saved board
+ */
+const addBoard = (board: IDraftBoard): IBoard | undefined=> {
     const newBoard = new Board(board).getBoard();
     return boardsRepo.addItem(newBoard);
 };
-
-const getAllBoards = (): (IBoard | undefined | null)[] => boardsRepo.getAllItems();
-
-const getBoard = (id: string) => boardsRepo.getItem(id);
-
-const updateBoard = (id: string, board: IBoard) => boardsRepo.updateItem(id, board);
-
-const isBoardExists = (id: string) => boardsRepo.checkItem(id);
-
-const deleteBoard = (id: string) => {
+/**
+ * Get all boards from repo
+ * 
+ * @returns array of boards
+ */
+const getAllBoards = (): (IBoard | undefined)[] => boardsRepo.getAllItems();
+/**
+ * Get board by id
+ * 
+ * @param id - string id of a board
+ * @returns a board from repo
+ */
+const getBoard = (id: string): IBoard | undefined => boardsRepo.getItem(id);
+/**
+ * Change fields of board in repo
+ * 
+ * @param id - id of a board
+ * @param board - board object with new values
+ * @returns 
+ */
+const updateBoard = (id: string, board: IBoard): IBoard | undefined => boardsRepo.updateItem(id, board);
+/**
+ * Check if board exists in repo
+ * 
+ * @param id - id of a board
+ * @returns - true if board exists or false
+ */
+const isBoardExists = (id: string): boolean => boardsRepo.checkItem(id);
+/**
+ * Delete board in repo and delete all related tasks
+ * 
+ * @param id - id of a board
+ */
+const deleteBoard = (id: string): void => {
     boardsRepo.deleteItem(id);
     tasksRepo.getTasksRepo(id).deleteAllItems();
 };
